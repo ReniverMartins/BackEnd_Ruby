efac8a772b905da64e795886e2cb747eba9dd955
Reniver Martins <r.kennyster@icloud.com>
2023-02-21 21:42:58 -0300

    Entrega do desafio de codigo 1

---
+++ Mod1/desafioCodigo.rb
@@ -0,0 +1,15 @@
+puts(".:: Seja bem vindoao cadastro de usuarios ::.")
+puts
+
+    print("Digite seu primeiro nome: ")
+    primNome= gets.chomp()
+
+    print("Digite seu segundo nome: ")
+    secNome = gets.chomp()
+
+    print("Digite sua idade: ")
+    idade = gets.chomp()
+
+    puts
+    puts(".::: Usuario Cadastrado com sucesso!! :::.")
+    puts("#{"Nome:"+primNome+" Sobrenome:"+secNome+" Idade:"+idade+" anos"}")
